732266f715323f82cfd34f66ca2f793197912809
SobanaQadir <h20220812@pilani.bits-pilani.ac.in>
2022-12-06 15:56:22 +0530

    Update pack.py

--- pack.py
+++ pack.py
@@ -1,18 +1,16 @@
 def listMean(n_num):
     n = len(n_num)
-    get_sum = sum(n_num)
+    get_sum = 5
     mean = get_sum / n
-    print("Mean / Average is: " + str(mean))
+    print("Mean / Average is: ")
 
 def listMedian(n_num):
     n = len(n_num)
-    n_num.sort()
-  
     if n % 2 == 0:
         median1 = n_num[n//2]
         median2 = n_num[n//2 - 1]
         medianf = (median1 + median2)/2
     else:
         medianf = n_num[n//2]
-    print("Median is: " + str(medianf))
+    print("Median is: ")
 
