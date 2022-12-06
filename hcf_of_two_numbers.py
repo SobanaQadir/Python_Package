11ca63d202f91b6ad2e32e388af4d0f427253e79
SobanaQadir <h20220812@pilani.bits-pilani.ac.in>
2022-12-06 16:00:14 +0530

    .

---
+++ hcf_of_two_numbers.py
@@ -0,0 +1,10 @@
+def calculate_hcf(x, y):  
+    # selecting the smaller number  
+    if x > y:  
+        smaller = y  
+    else:  
+        smaller = x  
+    for i in range(1,smaller + 1):  
+        if((x % i == 0) and (y % i == 0)):  
+            hcf = i  
+    return hcf  
