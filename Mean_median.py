1fc3aa3ecc49c980630ceaf086785d68b018b1d0
SobanaQadir <113101802+SobanaQadir@users.noreply.github.com>
2022-12-06 15:26:35 +0530

    Add files via upload

---
+++ Mean_median.py
@@ -0,0 +1,18 @@
+def Mean(n_num):
+    n = len(n_num)
+    get_sum = sum(n_num)
+    mean = get_sum / n
+    print("Mean / Average is: " + str(mean))
+
+def median(n_num):
+    n = len(n_num)
+    n_num.sort()
+  
+    if n % 2 == 0:
+        median1 = n_num[n//2]
+        median2 = n_num[n//2 - 1]
+        median = (median1 + median2)/2
+    else:
+        median = n_num[n//2]
+    print("Median is: " + str(median))
+
